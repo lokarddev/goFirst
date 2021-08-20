46d82c13a383206df15f6972fea3d058ba3e0ee1
Oleg Babenko <lokard.dev@gmail.com>
2021-08-20 17:35:06 +0300

    - set handler sand configs

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"github.com/spf13/viper"
 	"goFirst"
 	"goFirst/pkg/handler"
+	"goFirst/pkg/repository"
+	"goFirst/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("error while configuring")
+	}
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 	srv := new(goFirst.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occured while: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
